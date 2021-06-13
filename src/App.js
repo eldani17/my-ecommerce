32a4cbdc66ab6cbe75d1631b95630f59bf72107c
dandrademc <dandrade@mobilecomputing.com.ar>
2021-06-13 09:33:52 -0300

    remove file garbage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Mi Tienda en ReactJs</h1>
-    </div>
-  );
+import "./App.css";
+
+//Components
+import Header from "./components/header/Header";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Mi Tienda en ReactJs</h1>
+//     </div>
+//   );
+// }
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <h1>Hello Word</h1>
+      </>
+    );
+  }
 }
 
 export default App;
