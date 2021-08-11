535529d89af49ff1ee13aba02b68989a57a1ccdd
dandrademc <dandrade@mobilecomputing.com.ar>
2021-08-10 22:21:00 -0300

    add component count item product

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -4,9 +4,15 @@ import "./style.css";
 
 import Navbar from "../../components/navbar";
 import MainSection from "../../components/main-section";
+import ItemCount from "../../components/item-count";
 
 function Home() {
   const titles = ["The", "Classic - Collection", "Shop House"];
+
+  const onAddProduct = (quantity) => {
+    console.log(`execute onAddProduct, quantity product: ${quantity}`);
+  };
+
   return (
     <>
       <div className="container">
@@ -14,6 +20,9 @@ function Home() {
       </div>
       <div className="container-fluid">
         <MainSection greeting={titles} />
+      </div>
+      <div className="container">
+        <ItemCount stock={0} initial={5} onAdd={onAddProduct} />
       </div>
     </>
   );
