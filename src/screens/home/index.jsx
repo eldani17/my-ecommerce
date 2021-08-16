766e2e803734ef647ec68fbe5bd2b4a032a86fa8
dandrademc <dandrade@mobilecomputing.com.ar>
2021-08-16 19:14:29 -0300

    clean home

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -3,20 +3,14 @@ import React from "react";
 import "./style.css";
 
 import MainSection from "../../components/main-section";
-import ItemDetailContainer from "../../components/item-detail-container";
 
 function Home() {
   const titles = ["The", "Classic - Collection", "Shop House"];
 
   return (
-    <>
-      <div className="container-fluid">
-        <MainSection greeting={titles} />
-      </div>
-      <div className="container">
-        <ItemDetailContainer />
-      </div>
-    </>
+    <div className="container-fluid">
+      <MainSection greeting={titles} />
+    </div>
   );
 }
 
