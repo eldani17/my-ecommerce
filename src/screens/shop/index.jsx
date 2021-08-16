3d3cdfe9428eb792f53d05016329e7436de58439
dandrademc <dandrade@mobilecomputing.com.ar>
2021-08-16 18:54:32 -0300

    create screen shop

---
+++ src/screens/shop/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./style.css";
+
+import ItemListContainer from "../../components/item-list-container";
+
+function Shop() {
+  return (
+    <>
+      <div className="container">
+        <ItemListContainer />
+      </div>
+    </>
+  );
+}
+
+export default Shop;
