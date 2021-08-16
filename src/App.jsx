c37fadd484b09eb8aafbfb8aacacffadd4baf833
dandrademc <dandrade@mobilecomputing.com.ar>
2021-08-16 18:51:49 -0300

    add React Router

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+import Navbar from "./components/navbar";
+import Home from "./screens/home";
+import Shop from "./screens/shop";
+import NotFound from "./screens/not-found";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/shop">
+          <Shop />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
