f6ee439474a8fef7f673210afc5097b645f17ef4
dandrademc <dandrade@mobilecomputing.com.ar>
2021-08-16 18:53:23 -0300

    add screen handle path incorrect

---
+++ src/screens/not-found/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./style.css";
+
+function NotFound() {
+  return (
+    <>
+      <div className="container">
+        <h1>Not found</h1>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
