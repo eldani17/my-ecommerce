f060ac67da88fed17470a68cd0a4aa69ad0e075c
dandrademc <dandrade@mobilecomputing.com.ar>
2021-08-16 11:03:57 -0300

    add mock for simulator detail product

--- src/mocks/products-mock.js
+++ src/mocks/products-mock.js
@@ -63,3 +63,26 @@ export const getProductsMocks = () => {
     }, DELAY);
   });
 };
+
+const productByIdMock = {
+  id: 1,
+  title: "title 1",
+  price: 1000,
+  pictureUrl: "https://via.placeholder.com/650",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  picturesUrl: [
+    { pictureUrl: "https://via.placeholder.com/650" },
+    { pictureUrl: "https://via.placeholder.com/650" },
+    { pictureUrl: "https://via.placeholder.com/650" },
+    { pictureUrl: "https://via.placeholder.com/650" },
+  ],
+};
+
+export const getProductById = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(productByIdMock);
+    }, DELAY);
+  });
+};
