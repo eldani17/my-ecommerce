9d213a407fb2c66699fd6ddb399713108a56ee69
dandrademc <dandrade@mobilecomputing.com.ar>
2021-08-16 18:53:57 -0300

    remove navbar

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 import "./style.css";
 
-import Navbar from "../../components/navbar";
 import MainSection from "../../components/main-section";
-import ItemListContainer from "../../components/item-list-container";
 import ItemDetailContainer from "../../components/item-detail-container";
 
 function Home() {
@@ -12,14 +10,8 @@ function Home() {
 
   return (
     <>
-      <div className="container">
-        <Navbar />
-      </div>
       <div className="container-fluid">
         <MainSection greeting={titles} />
-      </div>
-      <div className="container">
-        <ItemListContainer />
       </div>
       <div className="container">
         <ItemDetailContainer />
